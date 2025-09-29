623607d971058113186847fa8abb77e2db350f4c
isliwei <49444387+isliwei@users.noreply.github.com>
2025-09-29 10:29:55 +0800

    feat: 补充及优化

--- controller/index.controller.js
+++ controller/index.controller.js
@@ -1,54 +1,62 @@
-const axios = require('axios')
+const axios = require("axios");
 
 class Index {
   async home(ctx, next) {
-    ctx.body = 'home page'
+    ctx.body = "home page";
   }
   async foo(ctx, next) {
-    ctx.body = 'foo page'
-    ctx.redirect('/bar') // 重定向，只针对同步请求
+    ctx.body = "foo page";
+    ctx.redirect("/bar"); // 重定向，只针对同步请求
   }
   async bar(ctx, next) {
     try {
-      ctx.body = 'bar page'
-      const xxx = JSON.parse('')
+      ctx.body = "bar page";
+      const xxx = JSON.parse("");
     } catch (e) {
-      throw ({ code: -2, message: e.message })
+      throw { code: -2, message: e.message };
     }
   }
   async getRemoteData(ctx, next) {
-    await axios.get('https://yunmk.feidee.net/cab-market-ws/market/v1/template/home?page_code=accountbook').then((res) => {
-      console.log(res.data.data)
-      ctx.body = {
-        code: 0,
-        result: res.data.data,
-        message: 'succeed'
-      }
-    })
+    await axios
+      .get(
+        "https://yunmk.feidee.net/cab-market-ws/market/v1/template/home?page_code=accountbook"
+      )
+      .then((res) => {
+        console.log(res.data.data);
+        ctx.body = {
+          code: 0,
+          result: res.data.data,
+          message: "succeed",
+        };
+      });
   }
   async render(ctx, next) {
-    await ctx.render('index', {
-      name: '张三',
-      list: ['number1', 'number2']
-    })
+    await ctx.render("index", {
+      name: "张三",
+      sex: "male",
+      list: ["number1", "number2"],
+    });
   }
   async upload(ctx, next) {
     try {
-      console.log()
-      const { file, file: { mimetype, newFilename } } = await ctx.request.files
+      console.log();
+      const {
+        file,
+        file: { mimetype, newFilename },
+      } = await ctx.request.files;
       ctx.body = {
         code: 0,
         result: {
           mimetype,
           fileName: newFilename,
-          url: `${ctx.request.host}/static/${newFilename} `
+          url: `${ctx.request.host}/static/${newFilename} `,
         },
-        message: '上传完成'
-      }
+        message: "上传完成",
+      };
     } catch (e) {
-      console.log(222, e)
+      console.log(222, e);
     }
   }
 }
 
-module.exports = new Index()
+module.exports = new Index();
--- router/index.route.js
+++ router/index.route.js
@@ -1,21 +1,32 @@
-const Router = require('@koa/router') // 路由
-const { koaBody } = require('koa-body') // post参数解析
-const path = require('path')
+const Router = require("@koa/router"); // 路由
+const { koaBody } = require("koa-body"); // post参数解析
+const path = require("path");
 
-const router = new Router({ prefix: '' })
-const { home, getRemoteData, foo, bar, render, upload } = require('../controller/index.controller')
+const router = new Router({ prefix: "" });
+const {
+  home,
+  getRemoteData,
+  foo,
+  bar,
+  render,
+  upload,
+} = require("../controller/index.controller");
 
-router.get('/', home)
-router.get('/getRemoteData', getRemoteData)
-router.get('/foo', foo)
-router.get('/bar', bar)
-router.get('/render', render)
-router.post('/upload', koaBody({
-  multipart: true,
-  formidable: {
-    uploadDir: path.join(__dirname, '../file'),
-    keepExtensions: true
-  }
-}), upload)
+router.get("/", home);
+router.get("/getRemoteData", getRemoteData);
+router.get("/foo", foo);
+router.get("/bar", bar);
+router.get("/render", render);
+router.post(
+  "/upload",
+  koaBody({
+    multipart: true, // 允许处理文件上传请求
+    formidable: {
+      uploadDir: path.join(__dirname, "../file"), // 上传文件存放的目录
+      keepExtensions: true, // 保留上传文件的原始扩展名
+    },
+  }),
+  upload
+);
 
-module.exports = router
+module.exports = router;
